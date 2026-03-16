60b0a90af06c3509a9c25121e721e5b59e8b98df
Quest-K <kjo36234311@gmail.com>
2026-03-16 17:28:26 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// [https://vite.dev/config/](https://vite.dev/config/)
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/Quest_K/', // 깃허브 페이지 배포를 위한 필수 설정입니다.
   server: {
-    host: '0.0.0.0',   // 모든 네트워크 인터페이스에서 접속 허용
+    host: '0.0.0.0',
     port: 3001,
-    strictPort: true,  // 포트 이미 사용 중이면 에러 (랜덤 포트 방지)
+    strictPort: true,
   },
-})
-
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/Quest_K/', // 이 부분이 누락되면 하얀 화면이 뜹니다.
 })
