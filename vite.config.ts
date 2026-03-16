9d6ad7d8fa6162e31c2c82780591435c4c91d013
Quest-K <kjo36234311@gmail.com>
2026-03-16 17:26:04 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,3 +11,12 @@ export default defineConfig({
     strictPort: true,  // 포트 이미 사용 중이면 에러 (랜덤 포트 방지)
   },
 })
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/Quest_K/', // 이 부분이 누락되면 하얀 화면이 뜹니다.
+})
